acb4dce9aab980f8c432c3d27b978de6b653c688
Nikita Shakarun <nikita36078@gmail.com>
2023-10-11 15:39:15 +0300

    Minor cleanup

--- include/DEComInterfaces_p.c
+++ include/DEComInterfaces_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.01.0626 */
-/* at Mon Jan 18 22:14:07 2038
+/* at Tue Jan 19 06:14:07 2038
  */
 /* Compiler settings for DEComInterfaces.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.01.0626 
